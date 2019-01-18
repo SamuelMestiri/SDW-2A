073dbb140e561576ba881f6a7ff1be1cbb81808f
SamuelMestiri <46810524+SamuelMestiri@users.noreply.github.com>
2019-01-18 13:00:35 +0100

    jquery + update script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("ok");
+$(document).ready(function(){
+
+    let w = parseFloat($("#carre").css("width").slice(0,-2));
+    let h = parseFloat($("#carre").css("height").slice(0,-2));
+    alert(w);
+
+
+    $("#petit").click(function () {
+
+         if (parseFloat($("#carre").css("height").slice(0,-2)) > 10 && parseFloat($("#carre").css("width").slice(0,-2)) > 10) {
+             $("#carre").css("width", (parseFloat($("#carre").css("width").slice(0,-2))/2)).css("height", (parseFloat($("#carre").css("height").slice(0,-2))/2));
+         }
+         else {
+             $("#carre").css("width", w).css("height", h);
+         }
+
+     });
+
+     $("#grand").click(function () {
+         $("#carre").css("width", parseFloat($("#carre").css("width").slice(0,-2))+10).css("height", parseFloat($("#carre").css("height").slice(0,-2))+10);
+     });
+
+});
